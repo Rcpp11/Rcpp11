922137ed99aff97b19d00e1b67b4f98ba5fd0f5b
Romain François <romain@r-enthusiasts.com>
2013-09-29 19:52:41 +0200

    indent

--- inst/include/Rcpp/traits/char_type.h
+++ inst/include/Rcpp/traits/char_type.h
@@ -26,14 +26,14 @@ namespace traits{
 
     template <typename T>
     struct char_type {
-    	typedef typename T::value_type type ; 
+        typedef typename T::value_type type ; 
     } ;
 	
     template <> struct char_type< const wchar_t* > {
-    	typedef wchar_t type ;	
+        typedef wchar_t type ;	
     } ;
     template <> struct char_type< const char* > {
-    	typedef char type ;
+        typedef char type ;
     } ;
     
 } // traits
