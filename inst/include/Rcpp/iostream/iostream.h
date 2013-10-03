b97ff9fb0313df32632a2afc9ed4aca6457cac8d
Romain François <romain@r-enthusiasts.com>
2013-10-03 21:33:39 +0200

    rollback a bit

---
+++ inst/include/Rcpp/iostream/iostream.h
@@ -0,0 +1,58 @@
+// Copyright (C) 2011 - 2012  Dirk Eddelbuettel, Romain Francois and Jelmer Ypma
+//
+// This file is part of Rcpp11.
+//
+// Rcpp11 is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Rcpp11 is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Rcpp11.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef RCPP__IOSTREAM__iostream_H
+#define RCPP__IOSTREAM__iostream_H
+
+#include <Rcpp/iostream/Rstreambuf.h>
+#include <Rcpp/iostream/Rostream.h>
+
+namespace Rcpp{ 
+    
+    // Rostream
+    template <> inline std::streamsize Rstreambuf<true>::xsputn(const char *s, std::streamsize num ) {
+        Rprintf( "%.*s", num, s ) ;
+        return num ;
+    }
+    template <> inline std::streamsize Rstreambuf<false>::xsputn(const char *s, std::streamsize num ) {
+        REprintf( "%.*s", num, s ) ; 
+        return num ;
+    }
+    
+    template <> inline int Rstreambuf<true>::overflow(int c ) {
+      if (c != EOF) Rprintf( "%.1s", &c ) ;
+      return c ;
+    }
+    template <> inline int Rstreambuf<false>::overflow(int c ) {
+      if (c != EOF) REprintf( "%.1s", &c ) ;
+      return c ;
+    }
+        
+    template <> inline int Rstreambuf<true>::sync(){
+        ::R_FlushConsole() ;
+        return 0 ;
+    }
+    template <> inline int Rstreambuf<false>::sync(){
+        ::R_FlushConsole() ;
+        return 0 ;
+    }
+    extern Rostream<true>  Rcout;
+    extern Rostream<false> Rcerr;
+
+    
+}
+#endif
