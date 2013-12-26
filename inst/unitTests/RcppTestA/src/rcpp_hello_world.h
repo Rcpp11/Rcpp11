9541d4f25be76b4f68e9102d8d994df0be471904
Romain Francois <romain@r-enthusiasts.com>
2013-12-26 17:09:47 +0100

    switch to Rcpp-test for tests

--- inst/unitTests/RcppTestA/src/rcpp_hello_world.h
+++
@@ -1,9 +0,0 @@
-#ifndef _RcppTestA_RCPP_HELLO_WORLD_H
-#define _RcppTestA_RCPP_HELLO_WORLD_H
-
-#include <Rcpp.h>
-
-extern "C" SEXP rcpp_hello_world() ;
-extern "C" SEXP hello_world_ex() ;
-
-#endif
