1f1a43fb1dae57968a3b59431e7b6551edcb73e8
Romain Francois <romain@r-enthusiasts.com>
2014-01-18 15:57:08 +0100

    More restrictive matching of c++ file extensions for compileAttributes. apply @jjallaire's fix to Rcpp11. closes #83

--- R/Attributes.R
+++ R/Attributes.R
@@ -1,20 +1,5 @@
 # Copyright (C) 2012 JJ Allaire, Dirk Eddelbuettel and Romain Francois
-#
-# This file is part of Rcpp11.
-#
-# Rcpp11 is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
-# (at your option) any later version.
-#
-# Rcpp11 is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Rcpp11.  If not, see <http://www.gnu.org/licenses/>.
-
+# Copyright (C) 2014 Romain Francois
 
 # Source C++ code from a file
 sourceCpp <- function(file = "",
@@ -351,8 +336,8 @@ compileAttributes <- function(pkgdir = ".", verbose = getOption("verbose")) {
         dir.create(rDir)
 
     # get a list of all source files
-    cppFiles <- list.files(srcDir, pattern=glob2rx("*.c*"))
-
+    cppFiles <- list.files(srcDir, pattern="\\.c(c|pp)$")
+    
     # derive base names (will be used for modules)
     cppFileBasenames <- tools::file_path_sans_ext(cppFiles)
 
