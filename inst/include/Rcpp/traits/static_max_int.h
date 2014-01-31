0c45c2dd0912f1761e007c78597924fea286c1e6
Kevin Ushey <kevinushey@gmail.com>
2014-01-30 23:56:53 -0800

    rearrange and remove forward declarations; just call it static_max for now (#92)

--- inst/include/Rcpp/traits/static_max_int.h
+++ inst/include/Rcpp/traits/static_max_int.h
@@ -5,28 +5,20 @@ namespace Rcpp {
   
 namespace traits {
 
-// forward declarations
-template <int T1, int T2, int... Args>
-constexpr int static_max_int__();
-
-template <int T1>
-constexpr int static_max_int__();
-
-// implementations
-template <int T1, int T2, int... Args>
-constexpr int static_max_int__() {
-  return T1 > T2 ? static_max_int__<T1, Args...>() : static_max_int__<T2, Args...>();
-}
-
 template <int T1>
 constexpr int static_max_int__() {
   return T1;
 }
 
+template <int T1, int T2, int... Args>
+constexpr int static_max_int__() {
+  return T1 > T2 ? static_max_int__<T1, Args...>() : static_max_int__<T2, Args...>();
+}
+
 // the struct to hold the value in a traits-like fashion
 // so we can do e.g. template_max<1, 2, 3>::value
 template <int T1, int T2, int... Args>
-struct static_max_int {
+struct static_max {
   enum { value = T1 > T2 ? static_max_int__<T1, Args...>() : static_max_int__<T2, Args...>() };
 };
 
