350873de973231497318b871fe2b91947b84152c
Kevin Ushey <kevinushey@gmail.com>
2014-02-15 21:56:18 -0800

    minor changes to modernize.sh

--- tools/modernize.sh
+++ tools/modernize.sh
@@ -1,9 +1,18 @@
+IN_RCPP=`pwd | grep Rcpp11$`
+if [ ${#IN_RCPP} == 0 ]; then
+    echo "Error: not in base Rcpp11 directory."
+    exit 0
+fi;
+
+## Put all of the header files into a file tool/modernize-include.txt
+## Don't know know, why clang-modernize isn't happy when we just path
+## the whole patch 'inst/include'
+find inst/include -name "*.h" -type f > tools/modernize-include.txt
+
 clang-modernize src/*.cpp \
-    -include="inst/include/Rcpp" \
-    -exclude="inst/include/Rcpp/iostream" \
+    -include-from=tools/modernize-include.txt \
     -style="Chromium" \
     -risk=risky \
-    -add-override \
     -pass-by-value \
     -use-auto \
     -- \
