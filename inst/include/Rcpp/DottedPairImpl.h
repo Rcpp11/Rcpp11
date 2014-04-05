1852557323d45f665e1c0ac57be81509a0265a0a
Romain François <romain@r-enthusiasts.com>
2014-04-05 19:36:56 +0200

    less meat

--- inst/include/Rcpp/DottedPairImpl.h
+++ inst/include/Rcpp/DottedPairImpl.h
@@ -8,26 +8,94 @@ namespace Rcpp{
     public:
         
         template <typename T>
-        Node push_back( const T& object) ;
+        Node push_front( const T& object){
+            CLASS& ref = get_ref() ;
+            ref.set__( grow(object, ref.get__() ) ) ;
+            return Node(ref.get__()) ;        
+        }
         
         template <typename T>
-        Node push_front( const T& object) ;
+        Node push_back( const T& object) {
+            CLASS& ref = get_ref() ;
+            if( ref.isNULL() ){
+                ref.set__( grow( object, ref.get__() ) ) ;
+                return Node( ref.get__() );
+            } else {                                       
+                SEXP x = ref.get__() ;
+                /* traverse the pairlist */
+                while( !Rf_isNull(CDR(x)) ){
+                    x = CDR(x) ;
+                }
+                Shield<SEXP> tail = pairlist( object ) ; 
+                SETCDR( x, tail ) ;
+                return Node(tail) ;
+            }    
+        }
         
         template <typename T>
-        Node insert( const size_t& index, const T& object) ;
+        Node insert( const size_t& index, const T& object) {
+            CLASS& ref = get_ref() ;
+            if( index == 0 ) {
+                return push_front( object ) ;
+            } else {
+                if( ref.isNULL( ) ) throw index_out_of_bounds() ;
+                
+                if( static_cast<R_len_t>(index) > ::Rf_length(ref.get__()) ) throw index_out_of_bounds() ;
+                
+                size_t i=1;
+                SEXP x = ref.get__() ;
+                while( i < index ){
+                	x = CDR(x) ;
+                	i++; 
+                }
+                Shield<SEXP> tail = grow( object, CDR(x) ) ; 
+                SETCDR( x, tail ) ;
+                return Node(tail) ;  
+            }
+                
+        }
         
         template <typename T>
-        Node replace( const int& index, const T& object ) ;
+        Node replace( const int& index, const T& object ) {
+            CLASS& ref = get_ref() ;
+            if( static_cast<R_len_t>(index) >= ::Rf_length(ref.get__()) ) throw index_out_of_bounds() ;
+              
+            Shield<SEXP> x = pairlist( object );
+            SEXP y = ref.get__() ;
+            int i=0;
+            while( i<index ){ y = CDR(y) ; i++; }
+            
+            SETCAR( y, CAR(x) );
+            SET_TAG( y, TAG(x) ); 
+            return Node(y) ;
+        }
         
         inline R_len_t length() const { 
-            return ::Rf_length(static_cast<const CLASS&>(*this).get__()) ; 
+            return ::Rf_length(get_ref().get__()) ; 
         }
         inline R_len_t size() const { 
-            return ::Rf_length(static_cast<const CLASS&>(*this).get__()) ;
+            return length() ;
         }
         
-        void remove( const size_t& index ); 
+        void remove( const size_t& index ){
+            CLASS& ref = static_cast<CLASS&>(*this) ;
+            if( static_cast<R_len_t>(index) >= Rf_length(ref.get__()) ) throw index_out_of_bounds() ;
+            if( index == 0 ){
+                ref.set__( CDR( ref.get__() ) ) ;
+            } else{
+                SEXP x = ref.get__() ;
+                size_t i=1;
+                while( i<index ){ x = CDR(x) ; i++; }
+                SETCDR( x, CDDR(x) ) ;
+            }
+    
+        }
 	    
+    private:
+        CLASS& get_ref(){
+            return static_cast<CLASS&>(*this) ;
+        }
+        
     } ;
     
 }
--- inst/include/Rcpp/internal/caster.h
+++ inst/include/Rcpp/internal/caster.h
@@ -2,34 +2,34 @@
 #define Rcpp__internal__caster__h
 
 namespace Rcpp{
-namespace internal{
-
-template <typename FROM, typename TO> TO caster(FROM from){
-	return static_cast<TO>(from) ;
-}
-template<> inline Rcomplex caster<std::complex<double>, Rcomplex>( std::complex<double> from){
-    	Rcomplex cx ;
-	cx.r = from.real() ; 
-	cx.i = from.imag() ;
-	return cx ;
-} 
-template<> inline Rcomplex caster<std::complex<float>, Rcomplex>( std::complex<float> from){
-	Rcomplex cx ;
-	cx.r = static_cast<double>( from.real() ); 
-	cx.i = static_cast<double>( from.imag() );
-	return cx ;
-}
-
-template<> inline std::complex<double> caster<Rcomplex,std::complex<double> >( Rcomplex from){
-    return std::complex<double>(from.r, from.i ) ;    
-}
-template<> inline std::complex<float>  caster<Rcomplex,std::complex<float>  >( Rcomplex from){
-    return std::complex<float>(static_cast<float>(from.r), static_cast<float>(from.i) ) ;    
-}
-
-template<> SEXP caster<String,SEXP>( String from ); 
-
-}
+    namespace internal{
+    
+        template <typename FROM, typename TO> TO caster(FROM from){
+            return static_cast<TO>(from) ;
+        }
+        template<> inline Rcomplex caster<std::complex<double>, Rcomplex>( std::complex<double> from){
+            Rcomplex cx ;
+            cx.r = from.real() ; 
+            cx.i = from.imag() ;
+            return cx ;
+        } 
+        template<> inline Rcomplex caster<std::complex<float>, Rcomplex>( std::complex<float> from){
+            Rcomplex cx ;
+            cx.r = static_cast<double>( from.real() ); 
+            cx.i = static_cast<double>( from.imag() );
+            return cx ;
+        }
+        
+        template<> inline std::complex<double> caster<Rcomplex,std::complex<double> >( Rcomplex from){
+            return std::complex<double>(from.r, from.i ) ;    
+        }
+        template<> inline std::complex<float>  caster<Rcomplex,std::complex<float>  >( Rcomplex from){
+            return std::complex<float>(static_cast<float>(from.r), static_cast<float>(from.i) ) ;    
+        }
+        
+        template<> SEXP caster<String,SEXP>( String from ); 
+    
+    }
 }
 
 #endif
