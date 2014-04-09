137edc1d016be6e00fa24eebdf92689a597fc7ad
Romain François <romain@r-enthusiasts.com>
2014-04-09 09:06:26 +0200

    using sugar iterators

--- inst/include/Rcpp/api/meat/sugar.h
+++ inst/include/Rcpp/api/meat/sugar.h
@@ -9,11 +9,7 @@ namespace Rcpp{
         template <typename Target, typename Expression>
         struct sugar_vector_expression_op {
             inline void apply( Target& target, const Expression& expr ){
-                int n = expr.size() ;
-                auto it = target.begin() ;
-                for(int i=0; i<n; i++, ++it){
-                    *it = expr[i] ;    
-                }        
+                std::copy( sugar_begin(expr), sugar_end(expr), target.begin() );
             }
         } ;
     }
