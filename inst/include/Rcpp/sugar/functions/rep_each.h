e63a82a67e69ae465e99093d206718ddaf3a582f
Kevin Ushey <kevinushey@gmail.com>
2014-04-08 22:28:20 -0700

    Update rep_each (#33)

--- inst/include/Rcpp/sugar/functions/rep_each.h
+++ inst/include/Rcpp/sugar/functions/rep_each.h
@@ -5,23 +5,48 @@ namespace Rcpp{
 namespace sugar{
 
 template <int RTYPE, bool NA, typename T>
-class Rep_each : public SugarVectorExpression< RTYPE ,NA, Rep_each<RTYPE,NA,T> > {
+class Rep_each :
+    public SugarVectorExpression< RTYPE ,NA, Rep_each<RTYPE,NA,T> >,
+    public custom_sugar_vector_expression {
+
 public:
+
     typedef typename Rcpp::VectorBase<RTYPE,NA,T> VEC_TYPE ;
     typedef typename Rcpp::traits::storage_type<RTYPE>::type STORAGE ;
 
-    Rep_each( const VEC_TYPE& object_, int times_ ) : 
-        object(object_), times(times_), n(object.size()) {}
+    Rep_each( const VEC_TYPE& object_, int times_ ) :
+        object(object_), times(times_), n(object_.size()) {}
 
     inline STORAGE operator[]( int i ) const {
         return object[ i / times ] ;
     }
     inline int size() const { return n * times ; }
 
+    template <typename Target>
+    inline void apply(Target& target) const {
+        return apply_impl(target, typename Rcpp::traits::is_materialized<T>::type() );
+    }
+
 private:
     const VEC_TYPE& object ;
-    int times; 
+    int times;
     int n ;
+
+    template <typename Target>
+    inline void apply_impl(Target& target, std::true_type) const {
+        auto it = target.begin();
+        for (int i=0; i < n; ++i) {
+            std::fill(it + i * times, it + (i + 1) * times, object[i]);
+        }
+    }
+
+    template <typename Target>
+    inline void apply_impl(Target& target, std::false_type) const {
+        auto it = target.begin();
+        for (int i=0; i < n; ++i) {
+            std::fill(it + i * times, it + (i + 1) * times, object[i]);
+        }
+    }
 } ;
 
 } // sugar
