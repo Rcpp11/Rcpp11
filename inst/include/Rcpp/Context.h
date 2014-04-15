c480c59b4858ed72ad19b31265a08eabda54b641
Romain François <romain@r-enthusiasts.com>
2014-04-15 18:51:51 +0200

    hide a few thuings from windows

--- inst/include/Rcpp/Context.h
+++ inst/include/Rcpp/Context.h
@@ -1,8 +1,81 @@
 #ifndef RCPP_CONTEXT_H
 #define RCPP_CONTEXT_H
 
+#if defined(_WIN32)
+
 namespace Rcpp {
+    
+    inline SEXP Rcpp_eval(SEXP expr, SEXP env ){
+        // version not using contexts for windows until we figure out how to
+        Shield<SEXP> x = Rf_lang3( Rf_install("try"), expr, Rf_ScalarLogical(TRUE) );
+        SET_TAG( CDDR(x), Rf_install("silent") );
+        Shield<SEXP> res = Rf_eval( x, env) ;
+        if( Rf_inherits( res, "try-error" ) ){
+            throw eval_error( CHAR(STRING_ELT(res, 0)) ) ;    
+        }
+        return res ;
+    }
+    inline SEXP get_current_call() { return R_NilValue; }
+}    
+    #else
  
+#define JMP_BUF sigjmp_buf
+
+enum {
+    CTXT_TOPLEVEL = 0,
+    CTXT_NEXT        = 1,
+    CTXT_BREAK      = 2,
+    CTXT_LOOP        = 3,    /* break OR next target */
+    CTXT_FUNCTION = 4,
+    CTXT_CCODE      = 8,
+    CTXT_RETURN      = 12,
+    CTXT_BROWSER  = 16,
+    CTXT_GENERIC  = 20,
+    CTXT_RESTART  = 32,
+    CTXT_BUILTIN  = 64  /* used in profiling */
+};
+                 
+#ifdef BC_INT_STACK
+typedef union { void *p; int i; } IStackval;
+#endif
+
+typedef struct RPRSTACK {
+    SEXP promise;
+    struct RPRSTACK *next;
+} RPRSTACK;
+
+typedef struct RCNTXT {
+    struct RCNTXT *nextcontext;    /* The next context up the chain */
+    int callflag;                    /* The context "type" */
+    JMP_BUF cjmpbuf;                /* C stack and register information */
+    int cstacktop;                  /* Top of the pointer protection stack */
+    int evaldepth;                /* evaluation depth at inception */
+    SEXP promargs;                /* Promises supplied to closure */
+    SEXP callfun;                    /* The closure called */
+    SEXP sysparent;                /* environment the closure was called from */
+    SEXP call;                        /* The call that effected this context*/
+    SEXP cloenv;                    /* The environment */
+    SEXP conexit;                    /* Interpreted "on.exit" code */
+    void (*cend)(void *);          /* C "on.exit" thunk */
+    void *cenddata;                  /* data for C "on.exit" thunk */
+    void *vmax;                      /* top of R_alloc stack */
+    int intsusp;                /* interrupts are suspended */
+    SEXP handlerstack;          /* condition handler stack */
+    SEXP restartstack;          /* stack of available restarts */
+    
+    struct RPRSTACK *prstack;   /* stack of pending promises */
+    SEXP *nodestack;
+    #ifdef BC_INT_STACK
+        IStackval *intstack;
+    #endif
+    SEXP srcref;                  /* The source line in effect */
+    int browserfinish;          /* should browser finish this context without stopping */
+} RCNTXT ;
+
+extern RCNTXT* R_GlobalContext;  
+    
+    namespace Rcpp{    
+    
     // borrowed from rJava
     inline SEXP get_current_call() {
         RCNTXT *ctx = R_GlobalContext;
@@ -70,27 +143,17 @@ namespace Rcpp {
         } 
     }
     
-    #if defined(_WIN32)
-    inline SEXP Rcpp_eval(SEXP expr, SEXP env ){
-        // version not using contexts for windows until we figure out how to
-        Shield<SEXP> x = Rf_lang3( Rf_install("try"), expr, Rf_ScalarLogical(TRUE) );
-        SET_TAG( CDDR(x), Rf_install("silent") );
-        Shield<SEXP> res = Rf_eval( x, env) ;
-        if( Rf_inherits( res, "try-error" ) ){
-            throw eval_error( CHAR(STRING_ELT(res, 0)) ) ;    
-        }
-        return res ;
-    }
-    #else
     inline SEXP Rcpp_eval(SEXP expr, SEXP env ){
         SEXP res ;  
         try_catch( [&](){
             res = Rf_eval(expr, env) ;        
         }) ;
         return res ;
-    }   
-    #endif
-  
+    } 
+    
+    
 }
+
+#endif
 
 #endif
