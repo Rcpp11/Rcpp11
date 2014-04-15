489bb4bfab6f09a55dfaeddc4c9870a608c0539f
Kevin Ushey <kevinushey@gmail.com>
2014-04-15 16:27:44 -0700

    Error on coerce by default when ctor-ing Vector from SEXP (#142)

--- inst/include/Rcpp/vector/impl/SimpleVector.h
+++ inst/include/Rcpp/vector/impl/SimpleVector.h
@@ -2,12 +2,12 @@
 #define Rcpp__vector__impl_SimpleVector_h
 
 namespace Rcpp{
-       
+
     #undef VEC
     #define VEC Vector<RTYPE,StoragePolicy>
-    
+
     template <
-        int RTYPE, 
+        int RTYPE,
         template <class> class StoragePolicy
     >
     class Vector :
@@ -16,12 +16,12 @@ namespace Rcpp{
         public SlotProxyPolicy<VEC>,
         public AttributeProxyPolicy<VEC>,
         public NamesProxyPolicy<VEC>,
-        public AttributesProxyPolicy<VEC>, 
-        public RObjectMethods<VEC>, 
+        public AttributesProxyPolicy<VEC>,
+        public RObjectMethods<VEC>,
         public VectorOffset<VEC>
     {
     public:
-        
+
         typedef typename traits::storage_type<RTYPE>::type value_type ;
         typedef value_type stored_type  ;
         typedef typename std::conditional<RTYPE==LGLSXP,bool,stored_type>::type init_type ;
@@ -29,81 +29,85 @@ namespace Rcpp{
         typedef const stored_type& const_reference ;
         typedef reference          Proxy           ;
         typedef const_reference    const_Proxy     ;
-        typedef value_type*        iterator        ; 
+        typedef value_type*        iterator        ;
         typedef const value_type*  const_iterator  ;
-        
+
         typedef internal::simple_name_proxy<RTYPE>         NameProxy       ;
         typedef internal::simple_const_name_proxy<RTYPE>   const_NameProxy ;
-        
+
         using VectorOffset<Vector>::size ;
-        
+
         RCPP_GENERATE_CTOR_ASSIGN(Vector)
-    
-        Vector( SEXP x ) {
+
+        Vector( SEXP x, bool coerce = false) {
+            if (!coerce && TYPEOF(x) != RTYPE) {
+                stop("Attempted to construct a '%s' from a vector of different type '%s'",
+                    CHAR(Rf_type2str(RTYPE)), CHAR(Rf_type2str(TYPEOF(x))));
+            }
             Storage::set__( r_cast<RTYPE>( x ) ) ;
         }
-        
+
         Vector(int n) {
             reset(n) ;
         }
-        
+
         Vector(){
             reset(0);
         }
-    
+
         Vector( int n, init_type x ) {
             reset(n);
             std::fill( begin(), end(), x) ;
         }
-        
+
         Vector( std::initializer_list<init_type> list ){
             reset(list.size());
             std::copy( list.begin(), list.end(), begin() ) ;
         }
-    
+
         template <bool NA, typename Expr>
         Vector( const SugarVectorExpression<RTYPE,NA,Expr>& other ) {
             reset(other.size()) ;
             other.apply(*this) ;
         }
-    
+
         template <bool NA, typename Expr>
         Vector& operator=( const SugarVectorExpression<RTYPE, NA, Expr>& expr ){
             int n = expr.size() ;
             if( n != size() ){
-                reset(n) ;    
+                reset(n) ;
             }
             expr.apply(*this) ;
             return *this ;
         }
-        
+
     private:
-        
+
         inline void reset(int n){
-            Storage::set__(Rf_allocVector(RTYPE, n) ) ;        
+            Storage::set__(Rf_allocVector(RTYPE, n) ) ;
         }
-        
+
         inline stored_type* data(){
-            return reinterpret_cast<stored_type*>( DATAPTR(Storage::get__()) );    
+            return reinterpret_cast<stored_type*>( DATAPTR(Storage::get__()) );
         }
         inline const stored_type* data() const{
-            return reinterpret_cast<const stored_type*>( DATAPTR(Storage::get__()) );    
+            return reinterpret_cast<const stored_type*>( DATAPTR(Storage::get__()) );
         }
-        
+
     public:
         inline iterator begin() { return data() ; }
         inline iterator end() { return data() + size() ; }
-        
+
         inline const_iterator begin() const{ return data() ; }
         inline const_iterator end() const{ return data() + size() ; }
-        
+
         inline Proxy operator[](int i){ return *( data() + i ) ;}
         inline const_Proxy operator[](int i) const { return *( data() + i ); }
-        
+
         template <typename... Args> static Vector create(Args... args) {
-            return typename create_type<RTYPE, Args...>::type( args... ) ;    
+            return typename create_type<RTYPE, Args...>::type( args... ) ;
         }
-    
+
         inline NameProxy operator[]( const std::string& name ){
             return NameProxy( *this, name ) ;
         }
@@ -113,7 +117,7 @@ namespace Rcpp{
     } ;
 
     #undef VEC
-    
+
 }
 
 
