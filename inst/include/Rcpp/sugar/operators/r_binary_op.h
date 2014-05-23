ed5a93ecec2900612b0703ceb1c6dd5997c8e6f9
Romain François <romain@r-enthusiasts.com>
2014-05-23 23:24:32 +0200

    no longer needing sugar::less, sugar::greater, ... generated by RCPP_OP macro

--- inst/include/Rcpp/sugar/operators/r_binary_op.h
+++ inst/include/Rcpp/sugar/operators/r_binary_op.h
@@ -65,29 +65,7 @@ namespace Rcpp{
                 >::type
             >::type type ;
         } ;
-        
-    namespace sugar {
-        
-        #undef RCPP_OP
-        #define RCPP_OP(NAME,OP)                                            \
-        template <int RTYPE>                                                \
-        class NAME {                                                        \
-        public:                                                             \
-            typedef typename traits::storage_type<RTYPE>::type STORAGE ;    \
-            inline int operator()( STORAGE lhs, STORAGE rhs) const {       \
-                return lhs OP rhs ;                                         \
-            }                                                               \
-        } ;
-        RCPP_OP(less,<)
-        RCPP_OP(greater,>)
-        RCPP_OP(less_or_equal,<=)
-        RCPP_OP(greater_or_equal,>=)
-        RCPP_OP(equal,==)
-        RCPP_OP(not_equal,!=)
-        #undef RCPP_OP
     
-    
-    } // sugar
 } // Rcpp
 
 #endif
