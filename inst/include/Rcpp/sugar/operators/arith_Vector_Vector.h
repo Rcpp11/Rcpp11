7d984ea570711d66ae8a1a1c882dd82ecfa1bb07
Romain François <romain@r-enthusiasts.com>
2014-05-24 00:02:18 +0200

    rm many Vector_Vector classes, all replaced by Arith_Vector_Vector

--- inst/include/Rcpp/sugar/operators/arith_Vector_Vector.h
+++ inst/include/Rcpp/sugar/operators/arith_Vector_Vector.h
@@ -10,5 +10,31 @@ operator+(
     return Rcpp::sugar::Arith_Vector_Vector<RTYPE,LHS_NA,LHS_T,RHS_NA,RHS_T, std::plus >( lhs, rhs ) ;
 }
 
+template <int RTYPE,bool LHS_NA, typename LHS_T, bool RHS_NA, typename RHS_T>
+inline Rcpp::sugar::Arith_Vector_Vector<RTYPE,LHS_NA,LHS_T,RHS_NA,RHS_T, std::minus >
+operator-( 
+    const Rcpp::SugarVectorExpression<RTYPE,LHS_NA,LHS_T>& lhs,
+    const Rcpp::SugarVectorExpression<RTYPE,RHS_NA,RHS_T>& rhs
+) {
+    return Rcpp::sugar::Arith_Vector_Vector<RTYPE,LHS_NA,LHS_T,RHS_NA,RHS_T, std::minus >( lhs, rhs ) ;
+}
+
+template <int RTYPE,bool LHS_NA, typename LHS_T, bool RHS_NA, typename RHS_T>
+inline Rcpp::sugar::Arith_Vector_Vector<RTYPE,LHS_NA,LHS_T,RHS_NA,RHS_T, std::multiplies >
+operator*( 
+    const Rcpp::SugarVectorExpression<RTYPE,LHS_NA,LHS_T>& lhs,
+    const Rcpp::SugarVectorExpression<RTYPE,RHS_NA,RHS_T>& rhs
+) {
+    return Rcpp::sugar::Arith_Vector_Vector<RTYPE,LHS_NA,LHS_T,RHS_NA,RHS_T, std::multiplies >( lhs, rhs ) ;
+}
+
+template <int RTYPE,bool LHS_NA, typename LHS_T, bool RHS_NA, typename RHS_T>
+inline Rcpp::sugar::Arith_Vector_Vector<RTYPE,LHS_NA,LHS_T,RHS_NA,RHS_T, std::divides >
+operator/( 
+    const Rcpp::SugarVectorExpression<RTYPE,LHS_NA,LHS_T>& lhs,
+    const Rcpp::SugarVectorExpression<RTYPE,RHS_NA,RHS_T>& rhs
+) {
+    return Rcpp::sugar::Arith_Vector_Vector<RTYPE,LHS_NA,LHS_T,RHS_NA,RHS_T, std::divides >( lhs, rhs ) ;
+}
 
 #endif
