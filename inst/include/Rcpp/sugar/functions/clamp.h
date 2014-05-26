2333e17cff1e3a744327c6df59b13c16e0ea683c
Romain François <romain@r-enthusiasts.com>
2014-05-26 17:15:23 +0200

    rewriting clamp with sapply. #183

--- inst/include/Rcpp/sugar/functions/clamp.h
+++ inst/include/Rcpp/sugar/functions/clamp.h
@@ -15,6 +15,7 @@ namespace Rcpp{
             }
             STORAGE lhs, rhs ;    
         } ;
+        
         // need to write this special version
         template <>
         struct clamp_operator<REALSXP,true> {
@@ -26,63 +27,16 @@ namespace Rcpp{
             }
             double lhs, rhs ;    
         } ;
-        
-        
-        
-        template <int RTYPE, bool NA, typename T>
-        class Clamp_Primitive_Vector_Primitive : public SugarVectorExpression< 
-            RTYPE , 
-            NA ,
-            Clamp_Primitive_Vector_Primitive<RTYPE,NA,T>
-        >, public custom_sugar_vector_expression {
-        public:
-            typedef typename Rcpp::traits::storage_type<RTYPE>::type STORAGE ;
-            typedef clamp_operator<RTYPE,NA> OPERATOR ;
-             
-            Clamp_Primitive_Vector_Primitive( STORAGE lhs_, const T& vec_, STORAGE rhs_) : vec(vec_), op(lhs_,rhs_) {}
-        
-            inline STORAGE operator[]( R_xlen_t i ) const {
-                return op( vec[i] ) ;
-            }
-            inline R_xlen_t size() const { return vec.size() ; }
-        
-            template <typename Target>
-            inline void apply(Target& target) const {
-                return apply_impl(target, typename traits::is_materialized<T>::type() ) ;    
-            }
-            
-        private:
-            const T& vec ;
-            OPERATOR op ;
-            
-            template <typename Target>
-            inline void apply_impl(Target& target, std::true_type) const {
-                std::transform( vec.begin(), vec.end(), target.begin(), op ) ;        
-            }
-            
-            template <typename Target>
-            inline void apply_impl(Target& target, std::false_type) const {
-                int n=size();
-                auto it = target.begin() ;
-                for(R_xlen_t i=0; i<n; i++, ++it){
-                    *it = op(vec[i]) ;
-                }
-            }
-            
-        } ;
-
-
 
     } // sugar
 
     template <int RTYPE, bool NA, typename T>
-    inline sugar::Clamp_Primitive_Vector_Primitive<RTYPE,NA,T> 
-    clamp( 
+    inline auto clamp( 
         typename Rcpp::traits::storage_type<RTYPE>::type lhs,
         const Rcpp::SugarVectorExpression<RTYPE,NA,T>& vec,  
         typename Rcpp::traits::storage_type<RTYPE>::type rhs
-        ){
-        return sugar::Clamp_Primitive_Vector_Primitive<RTYPE,NA,T>( lhs, vec.get_ref(), rhs ) ;
+    ) -> decltype( sapply( vec, sugar::clamp_operator<RTYPE,NA>(lhs, rhs) ) ) {
+        return sapply( vec, sugar::clamp_operator<RTYPE,NA>(lhs, rhs) ) ;
     }
 
 
