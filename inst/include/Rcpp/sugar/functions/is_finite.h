1104a051d10a77be8d3e9395f5af2a47876c11ac
Romain François <romain@r-enthusiasts.com>
2014-05-26 18:27:21 +0200

    reimplement is_finite on top of sapply. #183

--- inst/include/Rcpp/sugar/functions/is_finite.h
+++ inst/include/Rcpp/sugar/functions/is_finite.h
@@ -2,38 +2,10 @@
 #define Rcpp__sugar__is_finite_h
 
 namespace Rcpp{
-    namespace sugar{
-    
-        template <int RTYPE, bool NA, typename VEC_TYPE>
-        class IsFinite : 
-            public SugarVectorExpression< LGLSXP, false, IsFinite<RTYPE,NA,VEC_TYPE> >, 
-            public custom_sugar_vector_expression {
-        public:
-        
-            IsFinite( const SugarVectorExpression<RTYPE,NA,VEC_TYPE>& obj_) : obj(obj_){}
-        
-            inline int operator[]( R_xlen_t i ) const {
-                return ::Rcpp::traits::is_finite<RTYPE>( obj[i] ) ;
-            }
-        
-            inline R_xlen_t size() const { return obj.size() ; }
-                
-            template <typename Target>
-            inline void apply( Target& target ) const {
-                std::transform( sugar_begin(obj), sugar_end(obj), target.begin(), ::Rcpp::traits::is_finite<RTYPE> );  
-            }       
-            
-        private:
-            const SugarVectorExpression<RTYPE,NA,VEC_TYPE>& obj ;
-        
-        } ;
-    
-    
-    } // sugar
     
     template <int RTYPE, bool NA, typename T>
-    inline sugar::IsFinite<RTYPE,NA,T> is_finite( const Rcpp::SugarVectorExpression<RTYPE,NA,T>& t){
-        return sugar::IsFinite<RTYPE,NA,T>( t ) ;
+    inline auto is_finite( const Rcpp::SugarVectorExpression<RTYPE,NA,T>& t) -> decltype( sapply(t, Rcpp::traits::is_finite<RTYPE> ) ) {
+        return sapply(t, Rcpp::traits::is_finite<RTYPE> ) ;
     }
 
 } // Rcpp
