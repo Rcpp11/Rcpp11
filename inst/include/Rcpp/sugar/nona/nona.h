765b335ab076a11c17dde1105a56de1e96f79525
Romain Francois <romain@r-enthusiasts.com>
2014-06-01 07:38:14 +0200

    simplify noNA

--- inst/include/Rcpp/sugar/nona/nona.h
+++ inst/include/Rcpp/sugar/nona/nona.h
@@ -5,18 +5,22 @@ namespace Rcpp{
     namespace sugar {     
     
         template <typename Expr>
-        class Nona : public SugarVectorExpression<Nona<Expr>> {
+        class Nona : 
+            public SugarVectorExpression<Nona<Expr>>, 
+            public custom_sugar_vector_expression 
+        {
         public:
+            // TODO: do something to mark for the no NA property
             typedef typename Expr::value_type value_type ;
     
             Nona( const Expr& expr) : data(expr.get_ref()){} 
             
             inline R_xlen_t size() const { return data.size() ; }
-            inline STORAGE operator[](R_xlen_t i) const { return data[i] ; }
+            inline value_type operator[](R_xlen_t i) const { return data[i] ; }
             
             template <typename Target>
             void apply( Target& target ){
-                data.apply(target) ;  
+                data.get_ref().apply(target) ;  
             }
             
         private:
