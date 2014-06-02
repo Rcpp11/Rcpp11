42cf537f285d05a474b5c2a2029db44f3633bc30
Romain François <romain@r-enthusiasts.com>
2014-06-02 15:27:11 +0200

    impl Comparator using mapply

--- inst/include/Rcpp/sugar/operators/logical_operators__Vector__Vector.h
+++ inst/include/Rcpp/sugar/operators/logical_operators__Vector__Vector.h
@@ -2,65 +2,65 @@
 #define Rcpp__sugar__logical_operators__Vector__Vector_h
 
 /* Vector < Vector */ 
-template <int RTYPE,bool LHS_NA,typename LHS_T,bool RHS_NA, typename RHS_T>
-inline Rcpp::sugar::Comparator<RTYPE,LHS_NA, LHS_T, RHS_NA, RHS_T,std::less>
-operator<( 
-    const Rcpp::SugarVectorExpression<RTYPE,LHS_NA,LHS_T>& lhs , 
-    const Rcpp::SugarVectorExpression<RTYPE,RHS_NA,RHS_T>& rhs
-    ){
-    return Rcpp::sugar::Comparator<RTYPE,LHS_NA, LHS_T, RHS_NA, RHS_T,std::less>( lhs, rhs ) ;
+template <typename Expr1, typename Expr2>
+inline typename std::enable_if<
+    Rcpp::traits::same_mapply_scalar_type<Expr1,Expr2>::value, 
+    Mapply< std::less<typename Expr2::value_type>, Expr1, Expr2 >
+>::type
+operator<( const Rcpp::SugarVectorExpression<Expr1>& lhs , const Rcpp::SugarVectorExpression<Expr2>& rhs ){
+    return mapply( std::less<typename Expr1::value_type>(), lhs, rhs ) ;
 }
 
 /* Vector > Vector */
-template <int RTYPE,bool LHS_NA,typename LHS_T,bool RHS_NA, typename RHS_T>
-inline Rcpp::sugar::Comparator<RTYPE,LHS_NA, LHS_T, RHS_NA, RHS_T,std::greater>
-operator>( 
-    const Rcpp::SugarVectorExpression<RTYPE,LHS_NA,LHS_T>& lhs , 
-    const Rcpp::SugarVectorExpression<RTYPE,RHS_NA,RHS_T>& rhs
-    ){
-    return Rcpp::sugar::Comparator<RTYPE,LHS_NA, LHS_T, RHS_NA, RHS_T,std::greater>( lhs, rhs ) ;
+template <typename Expr1, typename Expr2>
+inline typename std::enable_if<
+    Rcpp::traits::same_mapply_scalar_type<Expr1,Expr2>::value, 
+    Mapply< std::greater<typename Expr2::value_type>, Expr1, Expr2 >
+>::type
+operator>( const Rcpp::SugarVectorExpression<Expr1>& lhs , const Rcpp::SugarVectorExpression<Expr2>& rhs ){
+    return mapply( std::greater<typename Expr1::value_type>(), lhs, rhs ) ;
 }
 
 /* Vector <= Vector */
-template <int RTYPE,bool LHS_NA,typename LHS_T,bool RHS_NA, typename RHS_T>
-inline Rcpp::sugar::Comparator<RTYPE,LHS_NA, LHS_T, RHS_NA, RHS_T,std::less_equal>
-operator<=( 
-    const Rcpp::SugarVectorExpression<RTYPE,LHS_NA,LHS_T>& lhs , 
-    const Rcpp::SugarVectorExpression<RTYPE,RHS_NA,RHS_T>& rhs
-    ){
-    return Rcpp::sugar::Comparator<RTYPE,LHS_NA, LHS_T, RHS_NA, RHS_T,std::less_equal>( lhs, rhs ) ;
+template <typename Expr1, typename Expr2>
+inline typename std::enable_if<
+    Rcpp::traits::same_mapply_scalar_type<Expr1,Expr2>::value, 
+    Mapply< std::less_equal<typename Expr2::value_type>, Expr1, Expr2 >
+>::type
+operator<=( const Rcpp::SugarVectorExpression<Expr1>& lhs , const Rcpp::SugarVectorExpression<Expr2>& rhs ){
+    return mapply( std::less_equal<typename Expr1::value_type>(), lhs, rhs ) ;
 }
 
 
 /* Vector >= Vector */
-template <int RTYPE,bool LHS_NA,typename LHS_T,bool RHS_NA, typename RHS_T>
-inline Rcpp::sugar::Comparator<RTYPE,LHS_NA, LHS_T, RHS_NA, RHS_T,std::greater_equal>
-operator>=( 
-    const Rcpp::SugarVectorExpression<RTYPE,LHS_NA,LHS_T>& lhs , 
-    const Rcpp::SugarVectorExpression<RTYPE,RHS_NA,RHS_T>& rhs
-    ){
-    return Rcpp::sugar::Comparator<RTYPE,LHS_NA, LHS_T, RHS_NA, RHS_T,std::greater_equal>( lhs, rhs ) ;
+template <typename Expr1, typename Expr2>
+inline typename std::enable_if<
+    Rcpp::traits::same_mapply_scalar_type<Expr1,Expr2>::value, 
+    Mapply< std::greater_equal<typename Expr2::value_type>, Expr1, Expr2 >
+>::type
+operator>=( const Rcpp::SugarVectorExpression<Expr1>& lhs , const Rcpp::SugarVectorExpression<Expr2>& rhs ){
+    return mapply( std::greater_equal<typename Expr1::value_type>(), lhs, rhs ) ;
 }
 
 
 /* Vector == Vector */
-template <int RTYPE,bool LHS_NA,typename LHS_T,bool RHS_NA, typename RHS_T>
-inline Rcpp::sugar::Comparator<RTYPE,LHS_NA, LHS_T, RHS_NA, RHS_T,std::equal_to>
-operator==( 
-    const Rcpp::SugarVectorExpression<RTYPE,LHS_NA,LHS_T>& lhs , 
-    const Rcpp::SugarVectorExpression<RTYPE,RHS_NA,RHS_T>& rhs
-    ){
-    return Rcpp::sugar::Comparator<RTYPE,LHS_NA, LHS_T, RHS_NA, RHS_T,std::equal_to>( lhs, rhs ) ;
+template <typename Expr1, typename Expr2>
+inline typename std::enable_if<
+    Rcpp::traits::same_mapply_scalar_type<Expr1,Expr2>::value, 
+    Mapply< std::equal_to<typename Expr2::value_type>, Expr1, Expr2 >
+>::type
+operator==( const Rcpp::SugarVectorExpression<Expr1>& lhs , const Rcpp::SugarVectorExpression<Expr2>& rhs ){
+    return mapply( std::equal_to<typename Expr1::value_type>(), lhs, rhs ) ;
 }
 
 /* Vector != Vector */
-template <int RTYPE,bool LHS_NA,typename LHS_T,bool RHS_NA, typename RHS_T>
-inline Rcpp::sugar::Comparator<RTYPE,LHS_NA, LHS_T, RHS_NA, RHS_T,std::not_equal_to>
-operator!=( 
-    const Rcpp::SugarVectorExpression<RTYPE,LHS_NA,LHS_T>& lhs , 
-    const Rcpp::SugarVectorExpression<RTYPE,RHS_NA,RHS_T>& rhs
-    ){
-    return Rcpp::sugar::Comparator<RTYPE,LHS_NA, LHS_T, RHS_NA, RHS_T,std::not_equal_to>( lhs, rhs ) ;
+template <typename Expr1, typename Expr2>
+inline typename std::enable_if<
+    Rcpp::traits::same_mapply_scalar_type<Expr1,Expr2>::value, 
+    Mapply< std::not_equal_to<typename Expr2::value_type>, Expr1, Expr2 >
+>::type
+operator!=( const Rcpp::SugarVectorExpression<Expr1>& lhs , const Rcpp::SugarVectorExpression<Expr2>& rhs ){
+    return mapply( std::not_equal_to<typename Expr1::value_type>(), lhs, rhs ) ;
 }
 
 
