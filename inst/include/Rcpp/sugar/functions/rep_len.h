90216c84b66b18d10e828f1c5702b05030ccd76b
Romain François <romain@r-enthusiasts.com>
2014-06-02 11:41:28 +0200

    rework rep_len

--- inst/include/Rcpp/sugar/functions/rep_len.h
+++ inst/include/Rcpp/sugar/functions/rep_len.h
@@ -2,64 +2,63 @@
 #define Rcpp__sugar__rep_len_h
 
 namespace Rcpp{
-namespace sugar{
-
-template <int RTYPE, bool NA, typename T>
-class Rep_len :
-    public SugarVectorExpression< RTYPE ,NA, Rep_len<RTYPE,NA,T> >,
-    public custom_sugar_vector_expression {
-
-public:
-    typedef typename Rcpp::SugarVectorExpression<RTYPE,NA,T> VEC_TYPE ;
-    typedef typename Rcpp::traits::storage_type<RTYPE>::type STORAGE ;
-
-    Rep_len( const VEC_TYPE& object_, int len_ ) :
-        object(object_.get_ref()), len(len_), n(object_.size()){}
-
-    inline STORAGE operator[]( R_xlen_t i ) const {
-        return object[ i % n ] ;
-    }
-    inline R_xlen_t size() const { return len ; }
-
-    template <typename Target>
-    inline void apply(Target& target) const {
-        if (n >= len) {
-            std::copy_n(sugar_begin(*this), len, target.begin() );
-            return;
-        }
-
-        int timesToFullCopy = len / n;
-        int leftoverElems = len % n;
+    namespace sugar{
+    
+        template <typename Expr>
+        class Rep_len :
+            public SugarVectorExpression<Rep_len<Expr>>,
+            public custom_sugar_vector_expression {
         
-        // first copy data from the sugar expression
-        auto it = target.begin();
-        std::copy_n(sugar_begin(*this), n, it);
-        it += n;
-                
-        // then copy the materialized part into the rest of the output
-        for (R_xlen_t i=1; i < timesToFullCopy; ++i) {
-            std::copy_n(target.begin(), n, it);
-            it += n;
+        public:
+            typedef typename Expr::value_type value_type ;
             
-        }
-        if (leftoverElems) {
-            std::copy_n(target.begin(), leftoverElems, it);
-        }
+            Rep_len( const VEC_TYPE& object_, R_xlen_t len_ ) :
+                object(object_), len(len_), n(object_.size()){}
         
+            inline value_type operator[]( R_xlen_t i ) const {
+                return object[ i % n ] ;
+            }
+            inline R_xlen_t size() const { return len ; }
+        
+            template <typename Target>
+            inline void apply(Target& target) const {
+                if (n >= len) {
+                    std::copy_n(sugar_begin(*this), len, target.begin() );
+                    return;
+                }
+        
+                int timesToFullCopy = len / n;
+                int leftoverElems = len % n;
+                
+                // first copy data from the sugar expression
+                auto it = target.begin();
+                std::copy_n(sugar_begin(*this), n, it);
+                it += n;
+                        
+                // then copy the materialized part into the rest of the output
+                for (R_xlen_t i=1; i < timesToFullCopy; ++i) {
+                    std::copy_n(target.begin(), n, it);
+                    it += n;
+                    
+                }
+                if (leftoverElems) {
+                    std::copy_n(target.begin(), leftoverElems, it);
+                }
+                
+            }
+        
+        private:
+            const SugarVectorExpression<Expr>& object ;
+            R_xlen_t len, n ;
+        
+        } ;
+    
+    } // sugar
+    
+    template <typename Expr>
+    inline sugar::Rep_len<Expr> rep_len( const SugarVectorExpression<Expr>& t, R_xlen_t len ){
+        return sugar::Rep_len<Expr>( t, len ) ;
     }
-
-private:
-    const T& object ;
-    int len, n ;
-
-} ;
-
-} // sugar
-
-template <int RTYPE, bool NA, typename T>
-inline sugar::Rep_len<RTYPE,NA,T> rep_len( const SugarVectorExpression<RTYPE,NA,T>& t, int len ){
-    return sugar::Rep_len<RTYPE,NA,T>( t, len ) ;
-}
 
 
 } // Rcpp
