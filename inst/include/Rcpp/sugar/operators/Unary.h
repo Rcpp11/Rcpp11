7413a9459beccbe577cae65dd310805972a7e887
Romain François <romain@r-enthusiasts.com>
2014-06-02 15:37:56 +0200

    cleanup

--- inst/include/Rcpp/sugar/operators/Unary.h
+++ inst/include/Rcpp/sugar/operators/Unary.h
@@ -1,5 +1,5 @@
-#ifndef Rcpp__sugar__Unary_h
-#define Rcpp__sugar__Unary_h
+#ifndef Rcpp__sugar__unary_Vector_h
+#define Rcpp__sugar__unary_Vector_h
 
 template <typename Expr>
 inline auto operator-( const Rcpp::SugarVectorExpression<Expr>& x ) -> decltype( sapply(x, std::negate<typename Expr::value_type>() ) ) {
--- inst/include/Rcpp/sugar/operators/operators.h
+++ inst/include/Rcpp/sugar/operators/operators.h
@@ -1,10 +1,6 @@
 #ifndef Rcpp__sugar__operators__operators_h
 #define Rcpp__sugar__operators__operators_h
 
-#include <Rcpp/sugar/operators/r_binary_op.h>
-#include <Rcpp/sugar/operators/r_arith_op.h>
-#include <Rcpp/sugar/operators/unary_op_type.h>
-
 // binary operators
 #include <Rcpp/sugar/operators/logical_operators__Vector__Vector.h> 
 #include <Rcpp/sugar/operators/logical_operators__Vector__primitive.h> 
@@ -15,6 +11,6 @@
 #include <Rcpp/sugar/operators/arith_Primitive_Vector.h>
 
 // unary operators
-#include <Rcpp/sugar/operators/Unary.h>
+#include <Rcpp/sugar/operators/unary_Vector.h>
 
 #endif
