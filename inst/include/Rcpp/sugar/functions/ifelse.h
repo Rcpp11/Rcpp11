871bfcfe19a46031fb75d89da0f11c48a9cf2ccb
Romain Francois <romain@r-enthusiasts.com>
2014-06-05 17:56:02 +0200

    gcc not happy

--- inst/include/Rcpp/sugar/functions/ifelse.h
+++ inst/include/Rcpp/sugar/functions/ifelse.h
@@ -3,31 +3,32 @@
 
 namespace Rcpp{
     namespace sugar{
-    
+
         struct ifelse_op {
-            
+
             template <typename T>
             inline T operator()(Rboolean cond, T lhs, T rhs ) const {
-                switch( cond ){ 
+                switch( cond ){
                     case TRUE: return lhs ;
                     case FALSE: return rhs ;
-                    case NA_VALUE: return NA ;
+                    default: break ;
                 } ;
+                return NA ;
             }
-            
+
             template <typename T>
             inline T operator()(bool cond, T lhs, T rhs ) const {
                 return cond ? lhs : rhs ;
             }
         } ;
-           
+
     }
-    
+
     template <typename Cond, typename Expr1, typename Expr2>
     inline auto ifelse( const Cond& cond, const Expr1& expr1, const Expr2& expr2 ) -> decltype(mapply( sugar::ifelse_op(), cond, expr1, expr2)) {
         return mapply( sugar::ifelse_op(), cond, expr1, expr2) ;
     }
-    
+
 } // Rcpp
 
 #endif
