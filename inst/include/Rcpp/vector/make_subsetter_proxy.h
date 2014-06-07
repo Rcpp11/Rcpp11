c7783b9fe9948481a49ae988e56534d224583bda
Romain François <romain@r-enthusiasts.com>
2014-06-07 17:12:58 +0200

    handle bool and Rboolean subsets

--- inst/include/Rcpp/vector/make_subsetter_proxy.h
+++ inst/include/Rcpp/vector/make_subsetter_proxy.h
@@ -3,16 +3,15 @@
 
 namespace Rcpp {
     
+    template <typename Source, typename eT, typename Expr>
+    struct subset_proxy_type {
+        typedef SubsetProxy<Source,int,IntegerVector> type ;
+    } ;
+
     template <typename Source, typename Expr>
     struct subset_proxy_type<Source, int, Expr> {
         typedef SubsetProxy<Source,int,Expr> type ;
     } ;
-    
-    template <typename Source, typename Expr>
-    struct subset_proxy_type<Source, String, Expr> {
-        typedef SubsetProxy<Source,int,IntegerVector> type ;
-    } ;
-    
     
     template <
         typename Source, // y
@@ -43,6 +42,39 @@ namespace Rcpp {
         }
         return SubsetProxy<Source,int,IntegerVector>( source, ind ) ;   
     }
+    
+    template <
+        typename Source, // y
+        typename Expr // x
+    >
+    inline typename subset_proxy_type<Source,Rboolean,Expr>::type 
+    make_subset_proxy( Source& source, const SugarVectorExpression<Rboolean, Expr>& index ){
+        auto n = index.size() ;
+        auto m = sum(index) ;
+        IntegerVector ind(m) ;
+        auto it = sugar_begin(index) ;
+        for( int i=0, k=0; i<n ; i++, ++it){
+            if( *it ) ind[k++] = i ;
+        }
+        return SubsetProxy<Source,int,IntegerVector>( source, ind ) ;   
+    }
+    
+    template <
+        typename Source, // y
+        typename Expr // x
+    >
+    inline typename subset_proxy_type<Source,bool,Expr>::type 
+    make_subset_proxy( Source& source, const SugarVectorExpression<bool, Expr>& index ){
+        auto n = index.size() ;
+        auto m = sum(index) ;
+        IntegerVector ind(m) ;
+        auto it = sugar_begin(index) ;
+        for( int i=0, k=0; i<n ; i++, ++it){
+            if( *it ) ind[k++] = i ;
+        }
+        return SubsetProxy<Source,int,IntegerVector>( source, ind ) ;   
+    }
+    
     
 }
 
