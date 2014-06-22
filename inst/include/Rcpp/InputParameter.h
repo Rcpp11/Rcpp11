a58346d6db93c14e1831f68c1f23925860590601
Romain François <romain@r-enthusiasts.com>
2014-06-22 23:58:56 +0200

    simplified InputParameter

--- inst/include/Rcpp/InputParameter.h
+++ inst/include/Rcpp/InputParameter.h
@@ -3,85 +3,58 @@
 
 namespace Rcpp { 
     
-    // default implementation used for pass by value
-    // as<> is called on the conversion operator
     template <typename T>
     class InputParameter {
     public:
-        typedef T type ;
-        InputParameter(SEXP x_) : x(x_){}
-        // InputParameter() : x(R_NilValue){}
+        InputParameter(SEXP x_) : 
+            x(x_){}
         
-        void set(SEXP x_){ x = x_ ; }
-        inline operator T() { return as<T>(x) ; }
-        inline T get() { return as<T>(x) ; }
-        
-        SEXP x ;
-    } ;
-    
-    // same for const
-    template <typename T>
-    class ConstInputParameter {
-    public:
-        typedef const T const_nonref ;
-        ConstInputParameter(SEXP x_) : x(x_) {}
-        
-        inline void set(SEXP x_){ x = x_; }
-        inline operator const_nonref() { return get() ; }
-        inline const_nonref get() { return as<T>(x) ; }
+        inline operator T() { 
+            return as<T>(x) ; 
+        }
         
         SEXP x ;
     } ;
     
-    
-    // impl for references. It holds an object at the constructor and then 
-    // returns a reference in the reference operator
     template <typename T>
-    class ReferenceInputParameter {
+    class InputParameter<const T> {
     public:
-        typedef T& reference ;
-        ReferenceInputParameter(SEXP x_) : x(x_), obj( as<T>(x_) ) {}
+        InputParameter(SEXP x_) : 
+            x(x_){}
         
-        void set(SEXP x_){ x = x_ ; obj = as<T>(x); }
-        inline operator reference() { return get() ; }
-        inline reference get(){ return obj; } 
-    
+        inline operator const T() { 
+            return as<T>(x) ; 
+        }
+        
         SEXP x ;
+    } ;
+    
+    template <typename T>
+    class InputParameter<T&> {
+    public:
+        InputParameter(SEXP x) : 
+            obj( as<T>(x) ) {}
+        
+        inline operator T&() { 
+            return obj ; 
+        }
+        
         T obj ; 
     } ;
 
-    // same for const references
     template <typename T>
-    class ConstReferenceInputParameter {
+    class InputParameter<const T&> {
     public:
-        typedef const T& const_reference ;
-        ConstReferenceInputParameter(SEXP x_) : x(x_), obj( as<T>(x_) ){}
-        inline operator const_reference() { return obj ; }
-        void set(SEXP x_){ x = x_ ; obj = as<T>(x); }
+        InputParameter(SEXP x) : 
+            obj( as<T>(x) ){}
+        
+        inline operator const T&() { 
+            return obj ; 
+        }
         
     private:
-        SEXP x ;
         T obj ;
     } ;
-    
-    namespace traits{
-        template <typename T>
-        struct input_parameter {
-            typedef typename Rcpp::InputParameter<T> type ;
-        } ;
-        template <typename T>
-        struct input_parameter<T&> {
-            typedef typename Rcpp::ReferenceInputParameter<T> type ;
-        } ;
-        template <typename T>
-        struct input_parameter<const T> {
-            typedef typename Rcpp::ConstInputParameter<T> type ;
-        } ;
-        template <typename T>
-        struct input_parameter<const T&> {
-            typedef typename Rcpp::ConstReferenceInputParameter<T> type ;
-        } ;
-    }    
     
 }
 
