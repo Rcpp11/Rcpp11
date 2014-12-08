7053d909d128365b5003b31f17bfaa71d1810ee9
Romain François <romain@r-enthusiasts.com>
2014-12-08 17:29:19 +0100

    fix unique

--- inst/include/Rcpp/sugar/functions/unique.h
+++ inst/include/Rcpp/sugar/functions/unique.h
@@ -5,14 +5,14 @@ namespace Rcpp{
 
     template <typename eT, typename Expr>
     inline typename traits::vector_of<eT>::type unique( const SugarVectorExpression<eT, Expr>& t ){
-        std::unordered_set<eT> set( t.begin(), t.end() ) ;
+        std::unordered_set<eT> set( sugar_begin(t), sugar_end(t) ) ;
         return import( set.begin(), set.end() ) ;
     }
     
     template <typename eT, typename Expr1, typename Expr2>
     inline LogicalVector in( const SugarVectorExpression<eT, Expr1>& x, const SugarVectorExpression<eT, Expr2>& table ){
-        std::unordered_set<eT> set( table.begin(), table.end() );
-        return transform( x.begin(), x.end(), [&set](eT y) -> Rboolean {
+        std::unordered_set<eT> set( sugar_begin(table), sugar_end(table) );
+        return transform( sugar_begin(x), sugar_end(x), [&set](eT y) -> Rboolean {
                 return set.count(y) ? TRUE : FALSE ;
         }) ;
     }
